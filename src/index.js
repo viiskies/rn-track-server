ddc73573804ebfe1886778f4c0ee9b4420082188
vytautas <v.sisla@gmail.com>
2020-11-24 14:02:15 +0200

    using postman

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
+
+app.use(authRoutes);
 
 const mongoUri =
   "mongodb+srv://admin:passwordpassword@cluster0.1l9jb.mongodb.net/<dbname>?retryWrites=true&w=majority";
